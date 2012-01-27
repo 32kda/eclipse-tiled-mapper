db5fbb5dda3d936b01f0b2e0aa34065efe58bfc9
dmitry.astu@gmail.com <dmitry.astu@gmail.com@c21ba9a5-2cc4-4edc-cef0-03755111aeb4>
2012-01-27 12:02:51 +0000

    Further actions/menus work

--- com.onpositive.mapper/src/com/onpositive/mapper/handlers/HighlightLayerActionHandler.java
+++ com.onpositive.mapper/src/com/onpositive/mapper/handlers/HighlightLayerActionHandler.java
@@ -1,27 +1,19 @@
 package com.onpositive.mapper.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.Command;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.commands.State;
-import org.eclipse.ui.IEditorPart;
 
 import com.onpositive.mapper.editors.MapEditor;
-import com.onpositive.mapper.ui.UIUtil;
 
-public class HighlightLayerActionHandler extends AbstractHandler {
+public class HighlightLayerActionHandler extends MapEditorActionHandler {
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IEditorPart activeEditor = UIUtil.getActiveEditor();
-		if (activeEditor != null && activeEditor instanceof MapEditor) {
-			Command command = event.getCommand();
-			State state = command.getState("STYLE");
-			state.setValue(!(Boolean) state.getValue());
-			((MapEditor) activeEditor).setHighlightCurrentLayer((Boolean) state.getValue());
-		}
-		return null;
+	public void executeForEditor(ExecutionEvent event, MapEditor activeEditor) {
+		Command command = event.getCommand();
+		State state = command.getState("STYLE");
+		state.setValue(!(Boolean) state.getValue());
+		activeEditor.setHighlightCurrentLayer((Boolean) state.getValue());
 	}
 
 }
--- com.onpositive.mapper/src/com/onpositive/mapper/handlers/SelectPreviousLayerHandler.java
+++ com.onpositive.mapper/src/com/onpositive/mapper/handlers/SelectPreviousLayerHandler.java
@@ -1,21 +1,13 @@
 package com.onpositive.mapper.handlers;
 
-import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.ui.IEditorPart;
 
 import com.onpositive.mapper.editors.MapEditor;
-import com.onpositive.mapper.ui.UIUtil;
 
-
-public class SelectPreviousLayerHandler  extends AbstractHandler{
+public class SelectPreviousLayerHandler  extends MapEditorActionHandler{
 
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		IEditorPart activeEditor = UIUtil.getActiveEditor();
-		if (activeEditor instanceof MapEditor)
-			((MapEditor) activeEditor).selectPrevLayer();
-		return null;
+	public void executeForEditor(ExecutionEvent event, MapEditor activeEditor) {
+		activeEditor.selectPrevLayer();		
 	}
 }
